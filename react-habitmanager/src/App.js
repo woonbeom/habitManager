2abeaa57d7b17d94c4e4033fcf6f96f09fe2d854
백운범 <woonbeom_baek@tmax.co.kr>
2020-01-15 20:46:28 +0900

    react 기본 개념 코멘트

--- react-habitmanager/src/App.js
+++ react-habitmanager/src/App.js
@@ -1,12 +1,19 @@
+// 컴포넌트들을 정의.
 import React, { Component } from 'react';
-import LoginPageTemplate from './components/Auth/LoginPageTemplate';
+import { Route } from 'react-router-dom';
+import { Home, Auth } from 'pages';
 
+// Component class 상속.
+// Component class는 컴포넌트의 기반이 되는 클래스.
+// JSX를 return.
+// React <- 컴포넌트를 정의하고 통합하여 렌더링 함.
 class App extends Component {
   render() {
     return (
-      <LoginPageTemplate>
-        템플릿 완성
-      </LoginPageTemplate>
+      <div>
+        <Route exact path="/" component={Home} />
+        <Route path="/auth" component={Auth} />
+      </div>
     );
   }
 }
--- react-habitmanager/src/index.js
+++ react-habitmanager/src/index.js
@@ -1,12 +1,20 @@
-import React from 'react';
+// 메인 프로그램.
+// import <- 외부 모듈 로드.
+// react, react-dom <- react 본체.
+// react는 ES6.
+import React from 'react'; 
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// App.js의 내용들을 가져와서 render.
+// ReactDOM.reder(컴포넌트, 조합 대상 요소);
+ReactDOM.render(<App />, document.getElementById('root')); // root 태그에 App 컴포넌트를 포함시킨다.
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+// JSX <- js 코드 내에서 html 태그를 사용할 수 있게 만드는 기능.
