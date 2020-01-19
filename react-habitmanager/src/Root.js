e48ea11d5b480ca53ae9dcfbc09f11ea503c3d6a
woonbeom <baek12201@gmail.com>
2020-01-19 22:44:20 +0900

    Root.js에 라우터 구성

--- react-habitmanager/src/Root.js
+++ react-habitmanager/src/Root.js
@@ -1,3 +1,13 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom'; // BrowserRouter로 라우터 구성.
 import App from './App';
+
+const Root = ({store}) => { // store <- 리덕스 스토어
+    return (
+        <BrowserRouter>
+            <Route path="/" component={App} />
+        </BrowserRouter>
+    );
+};
+
+export default Root;
