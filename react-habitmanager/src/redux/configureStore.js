397d37fc9089a3fd5894aaeca02360a84a10590d
woonbeom <baek12201@gmail.com>
2020-01-20 22:14:21 +0900

    리덕스에 모듈 핫로딩 적용

--- react-habitmanager/src/redux/configureStore.js
+++ react-habitmanager/src/redux/configureStore.js
@@ -1,9 +1,19 @@
 import { createStore } from 'redux';
 import modules from './modules';
 
+// TODO: 미들웨어 적용.
 // store 생성 함수를 configureStore.js에 저장.
 const configureStore = (initialState) => {
     const store = createStore(modules, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+    // hot-reloading를 위한 코드.
+    if (module.hot) {
+        module.hot.accept('./modules', () => {
+            const nextRootReducer = require('./modules').default;
+            store.replaceReducer(nextRootReducer);
+        });
+    }
+
     return store;
 }
 
